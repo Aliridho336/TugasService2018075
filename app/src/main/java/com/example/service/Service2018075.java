5a0bbfd18419ecf4cae50b55ac16a64e5bc342de
davifirman <115498002+Aliridho336@users.noreply.github.com>
2022-12-05 22:00:51 +0700

    new release

---
+++ app/src/main/java/com/example/service/Service2018075.java
@@ -0,0 +1,14 @@
+package com.example.service;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Service2018075 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service2018075);
+    }
+}
